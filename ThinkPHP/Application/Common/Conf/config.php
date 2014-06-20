0485ce83e468ba5c3d4e4e3d10a943bab3d9e6c1
zdfcumt <zdfcumt@yeah.net>
2014-06-20 14:11:02 +0800

    URL参数学习

--- ThinkPHP/Application/Common/Conf/config.php
+++ ThinkPHP/Application/Common/Conf/config.php
@@ -1,7 +1,10 @@
 <?php
 return array(
-  'QQ_APP_ID'=>1101507001,
-  'QQ_APP_KEY'=>'HTuhWLmogva94gi8',
+//  'QQ_APP_ID'=>1101507001,
+//  'QQ_APP_KEY'=>'HTuhWLmogva94gi8',
+  'QQ_APP_ID'=>1101517205,
+  'QQ_APP_KEY'=>'aIdxVqfr6vGnUDPD',
+
 
   'DB_TYPE'               =>  'mysql',     // 数据库类型
   'DB_HOST'               =>  '127.0.0.1', // 服务器地址
@@ -11,8 +14,14 @@ return array(
   'DB_PORT'               =>  '3306',        // 端口
   'DB_PREFIX'             =>  't1_',    // 数据库表前缀
  
+  'TMPL_TEMPLATE_SUBFIX'  => '.html',     //模版文件后缀 默认为.html
   'TMPL_PARSE_STRING'     => array(     //模版常量
     '__JS_PATH__' => __ROOT__.'/js',
+
+
+  'URL_HTML_SUBFIX'  => '.html',     //伪静态后缀 默认为.html
+
+  'DEFAULT_FILTER'  => 'htmlspecialchars' //URL查询参数过滤处理函数
   ),
 	//'配置项'=>'配置值'
 );
--- ThinkPHP/Application/Home/Controller/IndexController.class.php
+++ ThinkPHP/Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
   public function index(){
+    echo U('showGet', array('id'=>1, 'name'=>'name'), '.asp', false);
     $data=array(
       'qq_app_id'=>C(QQ_APP_ID),
       'qq_app_pf'=>'qzone',
@@ -11,6 +12,31 @@ class IndexController extends Controller {
     );
     $this->assign($data);
     
-    $this->display();
+    $this->display('index');
   }
+
+  public function createUrl(){
+    //(路径，查询参数，伪静态后缀，是否跳转)
+    echo U('Index/index', array('id'=>1, 'name'=>'name'), '.asp', false);
+    //(当前控制器路径，查询参数，伪静态后缀，是否跳转)
+    echo U('showGet', array('id'=>1, 'name'=>'name'), '.asp', false);
+  }
+
+  public function showGet(){
+    //_GET中添加了THINK的一些处理字段
+    print_r($_GET);
+    //从GET或POST中取得ID
+    echo I('id');
+    //取得GET数组
+    print_r(I('get.'));
+    //取得POST数组
+    print_r(I('post.'));
+    //I函数不会使用DEFAULT_FILTER处理查询参数，而$this->_post('');会,--_post函数不存在
+    //echo $this->_post('id');
+    //echo $this->_get('id');
+  }
+
+
+
+
 }
