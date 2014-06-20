3d27fa15605e83dbce44ebf738b0e8ffb938ee5b
zdfcumt <zdfcumt@yeah.net>
2014-06-20 17:28:29 +0800

    add db study

--- ThinkPHP/Application/Common/Conf/config.php
+++ ThinkPHP/Application/Common/Conf/config.php
@@ -6,22 +6,23 @@ return array(
   'QQ_APP_KEY'=>'aIdxVqfr6vGnUDPD',
 
 
-  'DB_TYPE'               =>  'mysql',     // 数据库类型
-  'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-  'DB_NAME'               =>  'tptest',          // 数据库名
-  'DB_USER'               =>  'root',      // 用户名
-  'DB_PWD'                =>  '1986',          // 密码
-  'DB_PORT'               =>  '3306',        // 端口
-  'DB_PREFIX'             =>  't1_',    // 数据库表前缀
+  'DB_TYPE'               =>  'mysql',        // 数据库类型
+  'DB_HOST'               =>  '127.0.0.1',    // 服务器地址
+  'DB_NAME'               =>  'tptest',       // 数据库名
+  'DB_USER'               =>  'root',         // 用户名
+  'DB_PWD'                =>  '1986',         // 密码
+  'DB_PORT'               =>  '3306',         // 端口
+  'DB_PREFIX'             =>  't1_',          // 数据库表前缀
  
-  'TMPL_TEMPLATE_SUBFIX'  => '.html',     //模版文件后缀 默认为.html
-  'TMPL_PARSE_STRING'     => array(     //模版常量
+  'TMPL_VAR_IDENTIFY'     => 'array',         //模板中的.只解析为数组，默认为数组或对象
+  'TMPL_TEMPLATE_SUBFIX'  => '.html',         //模版文件后缀 默认为.html
+  'TMPL_PARSE_STRING'     => array(           //模版常量
     '__JS_PATH__' => __ROOT__.'/js',
-
-
-  'URL_HTML_SUBFIX'  => '.html',     //伪静态后缀 默认为.html
-
-  'DEFAULT_FILTER'  => 'htmlspecialchars' //URL查询参数过滤处理函数
   ),
+
+
+  'URL_HTML_SUBFIX'       => '.html',               //伪静态后缀 默认为.html
+
+  'DEFAULT_FILTER'        => 'htmlspecialchars'     //URL查询参数过滤处理函数
 	//'配置项'=>'配置值'
 );
--- ThinkPHP/Application/Home/Controller/IndexController.class.php
+++ ThinkPHP/Application/Home/Controller/IndexController.class.php
@@ -7,8 +7,8 @@ class IndexController extends Controller {
     $data=array(
       'qq_app_id'=>C(QQ_APP_ID),
       'qq_app_pf'=>'qzone',
-      'user'=>dump(M('user')->select()),
-      'user2'=>fprint(M('user')->select()),
+      'user'=>M('user')->select(),
+      'user2'=>fprint(M('user')->select()), //定义于Common的funciont.php文件，会自动加载
     );
     $this->assign($data);
     
@@ -22,7 +22,25 @@ class IndexController extends Controller {
     echo U('showGet', array('id'=>1, 'name'=>'name'), '.asp', false);
   }
 
+  public function operDB(){
+    //查询
+    dump(M('user')->select());
+    //添加
+    $newuser=array('id'=>4, 'name'=>'amdin');
+    $uid = M('user')->data($newuser)->add();
+    if ($uid){
+      //$this->success("成功", U('index'));
+    }else{
+      //$this->error("失败");//跳转到上一个页面
+    }
+    //删除
+    //$deluser=array('id'=>array('eq',3));
+    $deluser='id = 2';
+    $ret = M('user')->where($deluser)->delete();//返回删除个数
+  }
+
   public function showGet(){
+    var_dump(IS_POST);
     //_GET中添加了THINK的一些处理字段
     print_r($_GET);
     //从GET或POST中取得ID
@@ -34,6 +52,17 @@ class IndexController extends Controller {
     //I函数不会使用DEFAULT_FILTER处理查询参数，而$this->_post('');会,--_post函数不存在
     //echo $this->_post('id');
     //echo $this->_get('id');
+
+
+    if (!IS_POST){
+      //这两个都没有了
+      //_404(提示内容，跳转地址),debug不会跳转
+      //$this->_404('页面不存在！', U('index'));
+      //错误地址
+      //halt('页面不存在');
+      echo "NOT POST METHOD";
+    }
+
   }
 
 
